4c33c3f827a722e949559296d12c7374482fb286
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-08-11 11:11:24 +0000

    added

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,6 +5,10 @@ export default function Contact() {
     <div id="contact">
       <br/>
       <h3 style={{margin:"0"}}>Contact: +91-726200971</h3>
+      <a target="_blank" href="https://www.amazon.in/b?_encoding=UTF8&tag=1234507f8-21&linkCode=ur2&linkId=341d7430885d6764f336724c61c92b2f&camp=3638&creative=24630&node=3561110031">IPHONE14</a>
+    <link rel = "icon" href = 
+    " http://localhost:3000/static/media/profile.2122bfb131f850846a6f.jpg" 
+       type = "image/x-icon"></link>
       <h3 style={{margin:"0"}}>shubhamtribhuvan8@gmail.com</h3>
     </div>
   )
